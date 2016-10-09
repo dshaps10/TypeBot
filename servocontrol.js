c0798c42656b025566c2c73b2bdf2e75099f1672
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-10-08 17:21:02 -0700

    Add initial files and set up basic servo control structure

---
+++ servocontrol.js
@@ -0,0 +1,20 @@
+var five = require('johnny-five');
+var positions = {};
+var servos = {};
+var opts;
+
+// Initialize servo controller module
+function init(board, options, callback) {
+
+}
+
+// Move the servos
+function move (destinations, callback) {
+
+}
+
+// Export the public methods
+module.exports = {
+  init: init,
+  move: move
+}
