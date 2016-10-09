86d42f9e619c22f5be4ebc749c0526eb2f6fb39d
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-10-08 17:32:35 -0700

    modify init function to include servo movement capabilities

--- servocontrol.js
+++ servocontrol.js
@@ -6,6 +6,30 @@ var opts;
 // Initialize servo controller module
 function init(board, options, callback) {
 
+  // Store options for use with move()
+  opts = options;
+
+  // Initialize the servos
+  for (var servo in options.servos) {
+
+    // Alias servo config for easy access
+    var servoConfig = options.servos[servo];
+
+    // Store start position as the current position
+    positions[servo] = servoConfig.startPosition;
+
+    // Create servo instance
+    servos[servo] = new five.Servo({
+      pin: servoConfig.pin,
+      isInverted: servoConfig.isInverted
+    });
+
+    // Move servo to starting position
+    servos[servo].to(positions[servo]);
+  }
+
+  // Wait for servos to move to starting positions
+  setTimeout(callback, 1000);
 }
 
 // Move the servos
